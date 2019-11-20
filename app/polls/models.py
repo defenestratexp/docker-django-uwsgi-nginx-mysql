948b6e53f0ca950357c763433fb9572760688d49
Troy Thompson <troycthompson@gmail.com>
2019-11-19 16:03:45 -0800

    models.py: added str to models

--- app/polls/models.py
+++ app/polls/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+# These come from the django polls tutoria
+# Documentation is here: https://docs.djangoproject.com/en/2.2/intro/tutorial02/
+
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+
+	# This method allows str translation of the object
+	def __str__(self):
+		return self.question_text
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+
+	# This method allows str translation of the object
+	def __str__(self):
+		return self.choice_text
