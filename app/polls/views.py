0ac49a0bf22e60330eb2f3bb0b3cd13458d5222c
Troy Thompson <troycthompson@gmail.com>
2019-11-20 17:19:48 -0800

    implement render shortcut for the index view

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,25 +1,16 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 
 # Create your views here.
-
-# Example of a very simple view
-#def index(request):
-#    return HttpResponse("Welcome to Nonagon Media")
 
 # This view displays the last 5 questions using the built-in
 # database API to retrieve the info and uses a template to
 # format the results
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	return HttpResponse(template.render(context, request))
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("This is question %s." % question_id)
