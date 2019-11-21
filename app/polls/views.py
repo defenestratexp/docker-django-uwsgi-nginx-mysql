e9d66aa9a56d29f9dad1e33604ba240525c221f6
Troy Thompson <troycthompson@gmail.com>
2019-11-20 16:58:31 -0800

    Additional views

--- app/polls/views.py
+++ app/polls/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to Nonagon Media")
+
+def detail(request, question_id):
+	return HttpResponse("This is question %s." % question_id)
+
+def results(request, question_id):
+	response = "The results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("Vote on question %s % question_id")
+
+
